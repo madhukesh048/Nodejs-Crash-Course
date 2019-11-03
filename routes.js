ec7b9d4fd29a4309b4e72a12e05ce8d4b297c880
Madhukesh <madhukesh04@gmail.com>
2019-11-03 11:55:38 +0530

    1-Improved Dev Workflow and Debugging

--- routes.js
+++ routes.js
@@ -33,7 +33,7 @@ const requestHandler = (req, res) => {
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My first Page</title></head>');
-    res.write('<body><h1>Hello world!!</h1></body>');
+    res.write('<body><h1>Hello world!!!</h1></body>');
     res.write('</html>');
     res.end();
 };
